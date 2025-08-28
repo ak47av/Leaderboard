8d6eef5135997c792c0392e38139180fbc1d107d
ak47av <sarunkarthikeyancgmhss@gmail.com>
2025-08-28 14:12:14 +0100

    added functionality to change rank

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 mod model;
 
-use model::{Leaderboard, Node};
+use model::{Leaderboard};
 
 fn main() {
     let mut leaderboard = Leaderboard::new();
 
-    leaderboard.insertNode("Hades".to_owned(), 1);
-    leaderboard.insertNode("Risebreak".to_owned(), 2);
-    leaderboard.insertNode("Witcher 3".to_owned(), 2);
-    leaderboard.insertNode("Sifu".to_owned(), 4);
-    leaderboard.insertNode("Read Dead Redemption 2".to_owned(), 1);
-    leaderboard.debug_pretty();
+    leaderboard.new_entry("Hades".to_owned(), 1);
+    leaderboard.new_entry("Risebreak".to_owned(), 2);
+    leaderboard.new_entry("Witcher 3".to_owned(), 2);
+    leaderboard.new_entry("God of War".to_owned(), 9);
+    leaderboard.new_entry("Read Dead Redemption 2".to_owned(), 1);
+
+    leaderboard.remove(89);
+    leaderboard.remove(5);
+    leaderboard.new_entry("Ori and the will of the wisps".to_owned(), 45);
+    leaderboard.change_rank(1, 4).unwrap();
     leaderboard.display();
 }
--- src/model.rs
+++ src/model.rs
@@ -1,14 +1,13 @@
-use std::collections::{HashMap};
 use std::cmp::Ordering;
 
-type ID = u32;
-type RANK = u32;
+type ID = usize;
+type RANK = usize;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Node {
     pub name: String,
     pub rank: RANK,
-    pub id: ID 
+    pub id: ID
 }
 
 impl Ord for Node {
@@ -34,6 +33,7 @@ impl PartialEq for Node {
 
 pub struct Leaderboard {
     entries: Vec<Node>,     // Sorted by ID
+    next_id: usize
 }
 
 impl Leaderboard {
@@ -41,21 +41,66 @@ impl Leaderboard {
     pub fn new() -> Self {
         Leaderboard {
             entries: Vec::new(),
+            next_id: 1
         }
     }
 
-    pub fn insertNode(&mut self, name: String, rank: u32){
-        let newID = self.entries.len() as u32 +1;
-        let newNode = Node {name: name, rank: rank, id: newID};
-        
+    fn insert_node_at_rank(&mut self, node: Node, rank: usize) -> Result<usize, String>{
+        if rank < 1 {
+            return Err(format!("Rank must be higher than 0"));
+        }
         for node in &mut self.entries {
             if rank <= node.rank {
                 node.rank += 1;
             }
         }
-
-        self.entries.push(newNode);
+        self.entries.push(node);
         self.entries.sort();
+        Ok(rank)
+    }
+
+    pub fn new_entry(&mut self, name: String, rank: usize) {
+        let rank = std::cmp::min(rank, self.entries.len()  + 1);
+        let new_node = Node {name: name.clone(), rank: rank, id: self.next_id};
+        self.next_id += 1;
+
+        match self.insert_node_at_rank(new_node, rank) {
+            Ok(r) => println!("{} successfully inserted at rank: {}", name, r),
+            Err(s) => println!("{}", s)
+        }
+    }
+
+    fn remove_node_by_rank(&mut self, rank: usize) -> Result<(String, usize), String> {
+        if (rank > self.entries.len() ) || (rank < 1) {
+            return Err(format!("No entry at Rank: {}, remove failed", rank));
+        }
+        let removed_name = self.entries[rank-1].name.clone();
+        self.entries.remove(rank  - 1);
+        for node in &mut self.entries {
+            if rank < node.rank {
+                node.rank -= 1;
+            }
+        }
+        Ok((removed_name, rank))
+    }
+
+    pub fn remove(&mut self, rank:usize) {
+        match self.remove_node_by_rank(rank) {
+            Ok((n, _r)) => println!("{} removed from rank: {}", n, rank),
+            Err(s) => println!("{}", s)
+        }
+    }
+
+    pub fn change_rank(&mut self, rank:usize, to_rank: usize) -> Result<usize, String> {
+        let mut temp = self.entries[rank-1].clone();
+        if let Err(err) = self.remove_node_by_rank(rank) {
+            return Err(format!("Change failed: {}", err));
+        }
+        temp.rank = to_rank;
+        if let Err(err) = self.insert_node_at_rank(temp, to_rank) {
+            return Err(format!("Change failed: {}", err));
+        }
+        Ok(to_rank)
     }
 
     pub fn debug_pretty(&self) {
